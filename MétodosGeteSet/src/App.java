187d1f05797925f6641244d1e447b0fbdd1e3417
Gerson Santos <gersones@outlook.com>
2024-04-19 20:41:00 -0300

    a

--- MétodosGeteSet/src/App.java
+++ MétodosGeteSet/src/App.java
@@ -1,7 +1,7 @@
-public class App 
+public class App {   
 
     public static void main(String[] args) {
             Aluno aluno1 = new Aluno();
             aluno1.setNome("Gerson");
             System.out.println(aluno1.getNome());
-}
+}}
