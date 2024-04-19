afe32ef726b4adfdd1246c241a4103da29a16ed3
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2024-04-19 17:46:31 -0300

    a

--- MétodosGeteSet/src/App.java
+++ MétodosGeteSet/src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+
+    public static void main(String[] args) {
+            Aluno aluno1 = new Aluno();
+            aluno1.setNome("Gerson");
+            System.out.println(aluno1.getNome());
 }
